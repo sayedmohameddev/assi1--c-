711914e6202442ee4d2d0642013a815eb5e824f6
sayed123 <164982798+sayedmohameddev@users.noreply.github.com>
2024-12-22 18:04:12 +0200

    Add files via upload

---
+++ assi 1/assi 1/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+
+
+class Program
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    static void Main()
+    {
+        //1
+
+        //Console.Write("Enter a number: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("You entered: " + number);
+
+
+
+        //2
+
+        //string input = "123abc";
+        //try
+        //{
+        //    int result = Convert.ToInt32(input);
+        //    Console.WriteLine(result);
+        //}
+        //catch (FormatException ex)
+        //{
+        //    Console.WriteLine("Error: Cannot convert string to an integer. " + ex.Message);
+        //}
+
+
+        //3
+
+        //float num1 = 5.7f;
+        //float num2 = 2.3f;
+        //float result = num1 / num2;
+        //Console.WriteLine("Result: " + result);
+
+
+        //4
+
+        //string str = "Hello, World!";
+        //string substring = str.Substring(7, 5); // Extracts "World"
+        //Console.WriteLine("Substring: " + substring);
+
+        //5
+
+        //int a = 10;
+        //int b = a;
+        //b = 20;
+        //Console.WriteLine($"a = {a}, b = {b}");
+
+
+        //6
+
+
+        //Person p1 = new Person { Name = "John" };
+        //Person p2 = p1;
+        //p2.Name = "Doe";
+        //Console.WriteLine($"p1.Name = {p1.Name}, p2.Name = {p2.Name}");
+
+
+        //7
+
+
+        //string str1 = "Hello";
+        //string str2 = "World";
+        //string combined = str1 + " " + str2;
+        //Console.WriteLine("Combined string: " + combined);
+
+
+
+        //8
+
+
+        //int d;
+        //d = Convert.ToInt32(!(30 < 20));
+
+
+        //9
+
+        //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+
+        //10
+
+
+        //int num = 1, z = 5;
+
+        //if (!(num <= 0))
+        //    Console.WriteLine(++num + z++ + " " + ++z);
+        //else
+        //    Console.WriteLine(--num + z-- + " " + --z);
+
+
+    }
+}
